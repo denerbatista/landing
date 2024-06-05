7a3794bc020aec200c52f47f4fe244a6783dc7b1
denerbatista <denerbatista@icloud.com>
2024-06-04 21:53:13 -0300

    feat: add Form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,19 @@
-import { useState } from "react";
 import "./App.css";
+import Form from "./components/Form";
 
 function App() {
-  // let name: string;
-  let [name, setName] = useState<string>();
-  let [cpf, setCpf] = useState<string>();
-  let [adress, setAdress] = useState<string>();
-  let [tel, setTel] = useState<number>();
-  let [country, setCountry] = useState<string>();
-
-
   return (
-    <>
-      <section>
-        <form action="submit">
-          <fieldset>
-            <label htmlFor="name">Nome:</label>
-            <input name='name' type="text" onChange={(e)=>setName(e.target.value)} value={name} />
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="cpf">CPF:</label>
-            <input name='cpf' type="text" onChange={(e)=>setCpf(e.target.value)} value={cpf} />
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="tel">Telefone:</label>
-            <input name='tel' type="number" onChange={(e)=>setName(e.target.value)} value={name}/>
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="Adress">Endereço:</label>
-            <input name='Adress' type="text" onChange={(e)=>setName(e.target.value)} value={name} />
-          </fieldset>
-
-          <fieldset>
-            <label htmlFor="country">Cidade:</label>
-            <input name='country' type="text" onChange={(e)=>setName(e.target.value)} value={name}/>
-          </fieldset>
-
-          <button type="submit">Enviar</button>
-        </form>
-      </section>
-    </>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: 'center',
+        width: "100vw",
+      }}
+    >
+      <Form />
+    </div>
   );
 }
 
---
+++ src/components/Form/index.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import './style.css'
+
+function Form() {
+  // const name: string;
+  const [name, setName] = useState<string>("");
+  const [cpf, setCpf] = useState<string>("");
+  const [adress, setAdress] = useState<string>("");
+  const [tel, setTel] = useState<number | string>("");
+  const [country, setCountry] = useState<string>("");
+
+  return (
+    <>
+      <section id="section-form">
+        <form action="submit">
+          <fieldset className="fieldset-form">
+            <label htmlFor="name">Nome:</label>
+            <input
+              name="name"
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+            />
+          </fieldset>
+
+          <fieldset className="fieldset-form">
+            <label htmlFor="cpf">CPF:</label>
+            <input
+              name="cpf"
+              type="text"
+              onChange={(e) => setCpf(e.target.value)}
+              value={cpf}
+            />
+          </fieldset>
+
+          <fieldset className="fieldset-form">
+            <label htmlFor="tel">Telefone:</label>
+            <input
+              name="tel"
+              type="number"
+              onChange={(e) => setTel(Number(e.target.value))}
+              value={tel}
+            />
+          </fieldset>
+
+          <fieldset className="fieldset-form">
+            <label htmlFor="Adress">Endereço:</label>
+            <input
+              name="Adress"
+              type="text"
+              onChange={(e) => setAdress(e.target.value)}
+              value={adress}
+            />
+          </fieldset>
+
+          <fieldset className="fieldset-form">
+            <label htmlFor="country">Cidade:</label>
+            <input
+              name="country"
+              type="text"
+              onChange={(e) => setCountry(e.target.value)}
+              value={country}
+            />
+          </fieldset>
+
+          <button type="submit">Enviar</button>
+        </form>
+
+        {/* Tag usada para manter a mesma estrutura de texto digitada */}
+        {/* <pre>
+          {`
+            Seu nome é ${name}
+            Seu cpf é ${cpf}
+            Você mora no endereço ${adress}
+            Sua cidade é ${country}
+            Seu telefone é ${tel}`}
+        </pre> */}
+
+        <div></div>
+      </section>
+    </>
+  );
+}
+
+export default Form;
